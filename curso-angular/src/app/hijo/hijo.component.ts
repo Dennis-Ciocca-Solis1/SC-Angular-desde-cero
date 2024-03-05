1921cdee10b4a6160c47714a7f7ebf8951fb5384
Dennis-Ciocca-Solis1 <dennisciocca@hotmail.com>
2024-03-05 16:48:59 -0500

    fin Comunicacion entre componentes 01:49:04

--- curso-angular/src/app/hijo/hijo.component.ts
+++ curso-angular/src/app/hijo/hijo.component.ts
@@ -1,30 +1,54 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-hijo',
-  templateUrl: './hijo.component.html',
-  styleUrls: ['./hijo.component.css']
+	selector: 'app-hijo',
+	templateUrl: './hijo.component.html',
+	styleUrls: ['./hijo.component.css']
 })
 export class HijoComponent {
-  
-  //este es el mensaje que recibimos del componente padre
-  // 9.4 Creando el recibeHijo e inicializándolo
-  // @Input() recibeHijo: string = '';
+	//este es el mensaje que recibimos del componente padre
+	// 9.4 Creando el recibeHijo e inicializándolo
+	// @Input() recibeHijo: string = '';
 
-  // 9.5 ir a hijo.component.html
-  // ---------------------------------------------------------
+	// 9.5 ir a hijo.component.html
+	// ---------------------------------------------------------
 
-  // 10.5 Declarando un 'emisor' desde el hijo hacia el padre
-  //importar EventEmitter
-  @Output() mensajeDesdeHijo = new EventEmitter<string>();
-  
-  // 10.6 Emitiendo el mensaje
-  mensaje: string = '';
-  //
-  enviarMensaje(){
-    this.mensajeDesdeHijo.emit(this.mensaje);
-  }
+	// 10.5 Declarando un 'emisor' desde el hijo hacia el padre
+	//importar EventEmitter
+	@Output() mensajeDesdeHijo = new EventEmitter<string>();
 
-  // 10.7 ir a hijo.component.html
+	// ---------------------------------------------------------
+	// 11.6 Realizando emisiones vacías
+	@Output() incrementarDesdeHijo = new EventEmitter<void>();
+	@Output() decrementarDesdeHijo = new EventEmitter<void>();
 
+	// 11.7 ir a hijo.component.html
+
+	// ---------------------------------------------------------
+	// 10.6 Emitiendo el mensaje
+	mensaje: string = '';
+	//
+	enviarMensaje() {
+		this.mensajeDesdeHijo.emit(this.mensaje);
+	}
+
+	// 10.7 ir a hijo.component.html
+
+	// ---------------------------------------------------------
+	// 11.4
+	// ++++++++++++++++++++++
+	// 11.10 reemplaza a 11.4
+	incrementar() {
+		this.incrementarDesdeHijo.emit();
+	}
+
+	decrementar() {
+		this.decrementarDesdeHijo.emit();
+	}
+
+  // 11.11 ir a padre.component.html
+
+	// ---------------------------------------------------------
+	// 11.5 Hacer otra emisión
+	// ir a la línea 22 de this.padre.component.ts
 }
--- curso-angular/src/app/padre/padre.component.ts
+++ curso-angular/src/app/padre/padre.component.ts
@@ -5,20 +5,31 @@ import { Component } from '@angular/core';
 	templateUrl: './padre.component.html',
 	styleUrls: ['./padre.component.css']
 })
-export class PadreComponent {
-  
-  //CONTADOR:
-	// 1. Crear variable ValorContador e inicializarla en 0
-	valorContador: number = 0;
-	// 2. ir a padre.component.html y agregar el valorContador
-
-  //----------------------------------------------------
+export class PadreComponent {	
 	// 9.2 Creando el 'mensajePadre'
 	// mensajePadre = 'Ojalá Messi llegue al próximo mundial';
 
-  // 9.3 luego ir a hijo.component.ts y crear un 'recibeHijo'
+	// 9.3 luego ir a hijo.component.ts y crear un 'recibeHijo'
 
-	//----------------------------------------------------
+	//---------------------------------------------------------------------
+	// 10. RECIBIENDO DATOS DEL HIJO EN EL PADRE
+	//crear un 'mensajeRecibido'
+	mensajeRecibido: string = '';
+
+	// 10.1 Método que se ejecutará con lo que mande el hijo por parámetro
+	recibirMensaje($event: string) {
+		this.mensajeRecibido = $event;
+	}
+
+	// 10.2 ir a padre.component.html
+
+	// -------------------------------------------------------------------
+	//CONTADOR:
+	// 1. Crear variable ValorContador e inicializarla en 0
+	valorContador: number = 0;
+	// 2. ir a padre.component.html y agregar el valorContador	
+
+	//---------------------------------------------------------------------
 	// 6. Creando los métodos de clase
 	incrementar() {
 		this.valorContador++;
@@ -27,19 +38,6 @@ export class PadreComponent {
 	decrementar() {
 		this.valorContador--;
 	}
+
 	// 7. Ir a crear los eventos 'click' en padre.component.html
-
-  //---------------------------------------------------------------------
-  // 10. RECIBIENDO DATOS DEL HIJO EN EL PADRE
-  //crear un 'mensajeRecibido'
-  mensajeRecibido: string = '';
-
-  // 10.1 Método que se ejecutará con lo que mande el hijo por parámetro
-  recibirMensaje($event: string) {
-    this.mensajeRecibido = $event;
-  }
-
-  // 10.2 ir a padre.component.html
-
-	
 }
